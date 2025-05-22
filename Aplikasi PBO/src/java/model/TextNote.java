bfeb945a33144f75084ed8c2645a2a418faf42a9
fransiscaanggr <fransiscaanggraeni2@gmail.com>
2025-05-22 17:39:46 +0700

    textnote

--- Aplikasi PBO/src/java/model/TextNote.java
+++ Aplikasi PBO/src/java/model/TextNote.java
@@ -12,19 +12,22 @@ public class TextNote extends Note {
     private String note;
     
     public TextNote(String note ) {
+        super(title, createdDate);
         this.note = note;
     }
 
     public void createNote() {
-        
+        System.out.println("Creating Text Note: " + getTitle() + ", at: " + getCreatedDate());
     }
     
     public void showNote() {
-        
+        System.out.println("Text Note: " + note);
     }
     
     public void editNote() {
-        
+        this.note = newNote;
+        System.out.println("Text Note edited: " + note);
+    }
     }
     
     public String getNote() {
